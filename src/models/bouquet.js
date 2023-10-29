4f752d8a1a5430ee7b573576d6358dc472916c82
Julius Gerba <juliusgerba@gmail.com>
2023-10-29 22:23:25 +0300

    schema upd

--- src/models/bouquet.js
+++ src/models/bouquet.js
@@ -2,11 +2,16 @@ const mongoose = require('mongoose');
 
 const bouquetSchema = new mongoose.Schema(
     {
-        title: { type: String },
-        price: { type: Number },
+        title: {
+            type: String,
+            trim: true,
+            maxlength: 25,
+            required: [true, 'Title field is empty'],
+        },
+        price: { type: Number, required: [true, 'Price field is empty'] },
         oldPrice: { type: Number },
-        description: { type: String },
-        src: { type: String },
+        description: { type: String, maxlength: 300 },
+        src: { type: String, required: [true, 'Src field is empty'] },
         flags: [String],
         new: { type: Boolean },
         sale: { type: Boolean },
